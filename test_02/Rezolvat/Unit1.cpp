b8cd13c0bb882f6065a008dfe6e2fc881b367147
Vladislav Shorodok <vlad.shorodok14@gmail.com>
2026-05-27 23:25:30 +0300

    Update Unit1.cpp

--- test_02/Rezolvat/Unit1.cpp
+++ test_02/Rezolvat/Unit1.cpp
@@ -4,261 +4,264 @@
 #pragma hdrstop
 #include <jpeg.hpp>
 #include "Unit1.h"
+
 //---------------------------------------------------------------------------
 #pragma package(smart_init)
 #pragma resource "*.dfm"
 TForm1* Form1;
-#include <jpeg.hpp>
 
-//---------------------------------------------------------------------------
-__fastcall TForm1::TForm1(TComponent* Owner) : TForm(Owner) {}
 TPoint pt[100];
 int cnt = 0;
-
+//---------------------------------------------------------------------------
+__fastcall TForm1::TForm1(TComponent* Owner) : TForm(Owner) {}
 //---------------------------------------------------------------------------
 void __fastcall TForm1::Button1Click(TObject* Sender)
 {
     Image1->Proportional = true;
     Image2->Proportional = true;
 
-
-    long Hr[256], Hg[256], Hb[256];
-    memset(Hr, 0, sizeof(Hr));
-    memset(Hg, 0, sizeof(Hg));
-    memset(Hb, 0, sizeof(Hb));
+    /// de completat
 
     if (OpenDialog1->Execute()) {
-        OpenDialog1->Filter = "Imagini |*bmp;*.jpg";
+        if (!FileExists(OpenDialog1->FileName)) {
+            return;
+        }
         AnsiString temp = ExtractFileExt(OpenDialog1->FileName);
         AnsiString Ext = temp.LowerCase();
         if (Ext.AnsiPos("jpg") > 0) {
-            TJPEGImage* jpeg = new TJPEGImage();
-            jpeg->LoadFromFile(OpenDialog1->FileName);
-            jpeg->DIBNeeded();
-            Image1->Picture->Bitmap->Assign(jpeg);
-            delete jpeg;
-
+            TJPEGImage* myjpeg = new TJPEGImage();
+            myjpeg->LoadFromFile(OpenDialog1->FileName);
+            myjpeg->DIBNeeded();
+            Image1->Picture->Bitmap->Assign(myjpeg);
+            delete myjpeg;
         } else if (Ext.AnsiPos("bmp") > 0) {
             Image1->Picture->Bitmap->LoadFromFile(OpenDialog1->FileName);
         }
     }
 
-    Graphics::TBitmap* src = new Graphics::TBitmap();
-    src->Assign(Image1->Picture->Bitmap);
-    src->PixelFormat = Image1->Picture->Bitmap->PixelFormat;
+    //** de completat histograma *//
+    long Hr[256], Hg[256], Hb[256];
+    memset(Hr, 0, sizeof(Hr));
+    memset(Hg, 0, sizeof(Hg));
+    memset(Hb, 0, sizeof(Hb));
 
-    for (int y = 0; y < src->Height; y++) {
-        RGBTRIPLE* pixels = (RGBTRIPLE*)src->ScanLine[y];
-        for (int x = 0; x < src->Width; x++) {
-            Hr[pixels[x].rgbtRed]++;
-            Hg[pixels[x].rgbtGreen]++;
-            Hb[pixels[x].rgbtBlue]++;
-        }
-    }
-    Chart1->Series[0]->Clear();
-    Chart1->Series[1]->Clear();
-    Chart1->Series[2]->Clear();
-    for (int i = 0; i < 256; i++) {
-        Chart1->Series[0]->AddY(Hr[i], " ", clRed);
-        Chart1->Series[1]->AddY(Hg[i], " ", clGreen);
-        Chart1->Series[2]->AddY(Hb[i], " ", clBlue);
-    }
-    Chart1->Series[0]->Active = true;
-    Chart1->Series[1]->Active = true;
-	Chart1->Series[2]->Active = true;
-}
-
-//---------------------------------------------------------------------------
-
-void __fastcall TForm1::Button2Click(TObject* Sender)
-{
     Graphics::TBitmap* src = new Graphics::TBitmap;
-
     src->Assign(Image1->Picture->Bitmap);
     src->PixelFormat = Image1->Picture->Bitmap->PixelFormat;
+    src->PixelFormat = Image1->Picture->Bitmap->PixelFormat;
 
-    // NIVELE GRI 
-    if (CheckBox5->Checked) {
-        double avg = 0;
-        for (int y = 0; y < src->Height; y++) {
-            RGBTRIPLE* pixelS = (RGBTRIPLE*)src->ScanLine[y];
-
-            for (int x = 0; x < src->Width; x++) {
-                avg = (pixelS[x].rgbtRed * 0.33 + pixelS[x].rgbtBlue * 0.33 +
-                       pixelS[x].rgbtGreen * 0.33);
-                pixelS[x].rgbtRed = avg;
-                pixelS[x].rgbtGreen = avg;
-                pixelS[x].rgbtBlue = avg;
-            }
-        }
-        if (!CheckBox1->Checked && !CheckBox3->Checked && !CheckBox1->Checked &&
-            !CheckBox2->Checked)
-        {
-            Image2->Picture->Bitmap->Assign(src);
-        }
-    }
     Graphics::TBitmap* dst = new Graphics::TBitmap;
     dst->Width = src->Width;
     dst->Height = src->Height;
     dst->PixelFormat = src->PixelFormat;
 
-    // SELECTIA 
+    for (int y = 0; y < src->Height; y++) {
+        for (int x = 0; x < src->Width; x++) {
+            RGBTRIPLE* pixels = (RGBTRIPLE*)src->ScanLine[y];
+
+            Hr[(int)(pixels[x].rgbtRed)]++;
+            Hg[(int)(pixels[x].rgbtGreen)]++;
+            Hb[(int)(pixels[x].rgbtBlue)]++;
+        }
+    }
+
+    Chart1->Series[0]->Clear();
+    Chart1->Series[1]->Clear();
+    Chart1->Series[2]->Clear();
+
+    for (int i = 0; i < 256; i++) {
+        Chart1->Series[0]->AddY(Hr[i], "", clRed);
+        Chart1->Series[1]->AddY(Hg[i], "", clGreen);
+        Chart1->Series[2]->AddY(Hb[i], "", clBlue);
+    }
+    Chart1->Series[0]->Active = true;
+    Chart1->Series[1]->Active = true;
+    Chart1->Series[2]->Active = true;
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Button2Click(TObject* Sender)
+{
+    Graphics::TBitmap* source = new Graphics::TBitmap;
+    source->Assign(Image1->Picture->Bitmap);
+    source->PixelFormat = Image1->Picture->Bitmap->PixelFormat;
+
+    // SELECTIA
     if (CheckBox3->Checked) {
-        int sx1 = pt[cnt - 2].x, sy1 = pt[cnt - 2].y;
-        int sx2 = pt[cnt - 1].X, sy2 = pt[cnt - 1].y;
+		int sx1 = pt[cnt - 2].x, sy1 = pt[cnt - 2].y;
+		int sx2 = pt[cnt - 1].x, sy2 = pt[cnt - 1].y;
 
-        if (sx1 > sx2) {
-            std::swap(sx1, sx2);
-        }
-        if (sy1 > sy2) {
-            std::swap(sy1, sy2);
-        }
+		if (sx1 > sx2) {
+			std::swap(sx1, sx2);
+		}
+		if (sy1 > sy2) {
+			std::swap(sy1, sy2);
+		}
 
-        int w = std::max(0, std::min(sx2, src->Width) - std::max(0, sx1));
-        int h = std::max(0, std::min(sy2, src->Height) - std::max(0, sy1));
+		int w = std::max(0, std::min(sx2, source->Width) - std::max(0, sx1));
+		int h = std::max(0, std::min(sy2, source->Width) - std::max(0, sy1));
 
-        if (w == 0 || h == 0) {
-            return;
-        }
-        Graphics::TBitmap* bmp = new Graphics::TBitmap();
-        bmp->PixelFormat = src->PixelFormat;
-        bmp->Width = w;
-        bmp->Height = h;
+		if (h == 0 || w == 0) {
+			return;
+		}
+		 Graphics::TBitmap * src1=Image1->Picture->Bitmap;
 
-        bmp->Canvas->CopyRect(
-            TRect(0, 0, w, h), src->Canvas, TRect(sx1, sy1, sx1 + w, sy1 + h));
-        Image2->Picture->Bitmap = bmp;
+		source->Width = w;
+		source->Height = h;
 
-        // FILTRARE
-    } else if (CheckBox1->Checked) {
-        double w[3][3] = { { 1.0 / 9, 1.0 / 9, 1.0 / 9 },
-            { 1.0 / 9, 1.0 / 9, 1.0 / 9 }, { 1.0 / 9, 1.0 / 9, 1.0 / 9 }
+		source->Canvas->CopyRect(
+			TRect(0, 0, w, h), src1->Canvas, TRect(sx1, sy1, sx1 + w, sy1 + h));
+		Image2->Picture->Bitmap->Assign(source);
+	}
 
-        };
-        for (int y = 0; y < dst->Height; y++) {
-            RGBTRIPLE* pixels = (RGBTRIPLE*)dst->ScanLine[y];
-            for (int x = 0; x < dst->Width; x++) {
-                double totalR = 0;
-                double totalG = 0;
-                double totalB = 0;
+    // NIVEL GRI
+    if (CheckBox5->Checked) {
+        double avg = 0;
+        for (int y = 0; y < source->Height; y++) {
+			RGBTRIPLE* pixelsS = (RGBTRIPLE*)source->ScanLine[y];
+
+			for (int x = 0; x < source->Width; x++) {
+				avg = (pixelsS[x].rgbtRed * 0.33 + pixelsS[x].rgbtGreen * 0.33 +
+                       pixelsS[x].rgbtBlue * 0.33);
+				pixelsS[x].rgbtRed = avg;
+				pixelsS[x].rgbtGreen = avg;
+				pixelsS[x].rgbtBlue = avg;
+			}
+		}
+		if (!CheckBox1->Checked && !CheckBox2->Checked && !CheckBox3->Checked) {
+			Image2->Picture->Bitmap->Assign(source);
+		}
+	}
+
+
+    Graphics::TBitmap* dest = new Graphics::TBitmap;
+    dest->Width = source->Width;
+    dest->Height = source->Height;
+    dest->PixelFormat = source->PixelFormat;
+
+    // FILTRARE
+    if (CheckBox1->Checked) {
+        double w[3][3] = { { 1.0 / 10, 1.0 / 10, 1.0 / 10 },
+            { 1.0 / 10, 1.0 / 10, 1.0 / 10 },
+            { 1.0 / 10, 1.0 / 10, 1.0 / 10 } };
+
+        for (int y = 0; y < dest->Height; y++) {
+            RGBTRIPLE* pixels = (RGBTRIPLE*)dest->ScanLine[y];
+            for (int x = 0; x < dest->Width; x++) {
+                double totalR = 0, totalG = 0, totalB = 0;
                 for (int i = 0; i < 3; i++) {
                     for (int j = 0; j < 3; j++) {
-                        int scX = x + j - 1;
-                        int scY = y + i - 1;
-                        if (scX >= 0 && scX < src->Width && scY >= 0 &&
-                            scY < src->Height) {
-                            RGBTRIPLE* pixelsS = (RGBTRIPLE*)src->ScanLine[scY];
-                            totalR += w[i][j] * pixelsS[scX].rgbtRed;
-                            totalG += w[i][j] * pixelsS[scX].rgbtGreen;
-                            totalB += w[i][j] * pixelsS[scX].rgbtBlue;
+                        int srcX = x + i - 1;
+                        int srcY = y + j - 1;
+                        if (srcX >= 0 && srcX < source->Width && srcY >= 0 &&
+                            srcY < source->Height) {
+                            RGBTRIPLE* pixelsS =
+                                (RGBTRIPLE*)source->ScanLine[srcY];
+                            totalR += w[i][j] * pixelsS[srcX].rgbtRed;
+                            totalG += w[i][j] * pixelsS[srcX].rgbtGreen;
+                            totalB += w[i][j] * pixelsS[srcX].rgbtBlue;
                         }
                     }
                 }
                 pixels[x].rgbtRed = std::max(0, std::min(255, (int)totalR));
                 pixels[x].rgbtGreen = std::max(0, std::min(255, (int)totalG));
                 pixels[x].rgbtBlue = std::max(0, std::min(255, (int)totalB));
-
-                Image2->Picture->Bitmap->Assign(dst);
             }
         }
+        Image2->Picture->Bitmap->Assign(dest);
     }
-    // DETECTAREA MUCHIILOR
+
+    // DETECTIA MUCHIILOR
     if (CheckBox2->Checked) {
-        int w[3][3] = { { 1, 1, 1 }, { 0, 0, 0 }, { -1, -1, -1 } };
-        int w1[3][3] = { { 1, 0, -1 }, { 1, 0, -1 }, { 1, 0, -1 } };
-        for (int y = 0; y < dst->Height; y++) {
-            RGBTRIPLE* pixels = (RGBTRIPLE*)dst->ScanLine[y];
-            for (int x = 0; x < dst->Width; x++) {
-                double totalR = 0;
-                double totalG = 0;
-                double totalB = 0;
-                double totalR1 = 0;
-                double totalG1 = 0;
-                double totalB1 = 0;
+        double w[3][3] = { { 0, 0, 0 }, { 1, -1, 0 }, { 0, 0, 0 } };
+        double w1[3][3] = { { 0, 1, 0 }, { 0, -1, 0 }, { 0, 0, 0 } };
+
+        for (int y = 0; y < dest->Height; y++) {
+            RGBTRIPLE* pixels = (RGBTRIPLE*)dest->ScanLine[y];
+            for (int x = 0; x < dest->Width; x++) {
+                double totalR = 0, totalG = 0, totalB = 0;
+                double totalR1 = 0, totalG1 = 0, totalB1 = 0;
                 for (int i = 0; i < 3; i++) {
                     for (int j = 0; j < 3; j++) {
-                        int scX = x + j - 1;
-                        int scY = y + i - 1;
-                        if (scX >= 0 && scX < src->Width && scY >= 0 &&
-                            scY < src->Height) {
-                            RGBTRIPLE* pixelsS = (RGBTRIPLE*)src->ScanLine[scY];
-                            totalR += w[i][j] * pixelsS[scX].rgbtRed;
-                            totalG += w[i][j] * pixelsS[scX].rgbtGreen;
-                            totalB += w[i][j] * pixelsS[scX].rgbtBlue;
-                            totalR1 += w1[i][j] * pixelsS[scX].rgbtRed;
-                            totalG1 += w1[i][j] * pixelsS[scX].rgbtGreen;
-                            totalB1 += w1[i][j] * pixelsS[scX].rgbtBlue;
+                        int srcX = x + i - 1;
+                        int srcY = y + j - 1;
+                        if (srcX >= 0 && srcX < source->Width && srcY >= 0 &&
+                            srcY < source->Height) {
+                            RGBTRIPLE* pixelsS =
+                                (RGBTRIPLE*)source->ScanLine[srcY];
+                            totalR += w[i][j] * pixelsS[srcX].rgbtRed;
+                            totalG += w[i][j] * pixelsS[srcX].rgbtGreen;
+                            totalB += w[i][j] * pixelsS[srcX].rgbtBlue;
+
+                            totalR1 += w1[i][j] * pixelsS[srcX].rgbtRed;
+                            totalG1 += w1[i][j] * pixelsS[srcX].rgbtGreen;
+                            totalB1 += w1[i][j] * pixelsS[srcX].rgbtBlue;
                         }
                     }
                 }
-                pixels[x].rgbtRed =
-                    255 - std::max(0,
-                              std::min(255,
-                                  (int)sqrt(pow(totalR, 2) + pow(totalR1, 2))));
-                pixels[x].rgbtGreen =
-                    255 - std::max(0,
-                              std::min(255,
-                                  (int)sqrt(pow(totalG, 2) + pow(totalG1, 2))));
-                pixels[x].rgbtBlue =
-                    255 - std::max(0,
-                              std::min(255,
-                                  (int)sqrt(pow(totalB, 2) + pow(totalB1, 2))));
-
-                Image2->Picture->Bitmap->Assign(dst);
+                pixels[x].rgbtRed = std::max(
+                    0, std::min(255,
+                           (int)sqrt(totalR * totalR + totalR1 * totalR1)));
+                pixels[x].rgbtGreen = std::max(
+                    0, std::min(255,
+                           (int)sqrt(totalG * totalG + totalG1 * totalG1)));
+                pixels[x].rgbtBlue = std::max(
+                    0, std::min(255,
+                           (int)sqrt(totalB * totalB + totalB1 * totalB1)));
             }
         }
+        Image2->Picture->Bitmap->Assign(dest);
+	}
 
-        // BINARIZARE
-    } else if (CheckBox4->Checked) {
-        double avg = 0;
-        for (int y = 0; y < src->Height; y++) {
-            RGBTRIPLE* pixelS = (RGBTRIPLE*)src->ScanLine[y];
-            for (int x = 0; x < src->Width; x++) {
-                avg = (pixelS[x].rgbtRed * 0.33 + pixelS[x].rgbtBlue * 0.33 +
-                       pixelS[x].rgbtGreen * 0.33);
-                if (Edit1->Text.ToIntDef(0) * 255.0 / 100 > avg) {
-                    dst->Canvas->Pixels[x][y] = TColor(RGB(0, 0, 0));
-                } else {
-                    dst->Canvas->Pixels[x][y] = TColor(RGB(255, 255, 255));
+    // BINARIZARE
+	if (CheckBox4->Checked) {
+	   double avg = 0;
+	   for (int y = 0; y < source->Height; y++) {
+		   RGBTRIPLE* pixelsS = (RGBTRIPLE*)source->ScanLine[y];
+		   for (int x = 0; x < source->Width; x++) {
+				avg = (pixelsS[x].rgbtRed * 0.33 + pixelsS[x].rgbtGreen * 0.33 + pixelsS[x].rgbtBlue);
+				if (Edit1->Text.ToIntDef(0) * 255.0 / 100 > avg) {
+					dest->Canvas->Pixels[x][y] = TColor(RGB(0, 0, 0));
+				} else {
+					dest->Canvas->Pixels[x][y] = TColor(RGB(255, 255, 255));
                 }
-            }
-        }
-		Image2->Picture->Bitmap->Assign(dst);
+		   }
+	   }
+	}
 
-        // MOZAIC
-    } else if (CheckBox6->Checked) {
-        int mozaicSize=8;
-		for(int y=0;y<src->Height;y++)
-		{
-			for(int x=0;x<src->Width;x++)
-			{
-				int newX=mozaicSize-x%mozaicSize;
-				int newY=mozaicSize-y%mozaicSize;
-
-				if(x+newX>0 && x+newX<src->Width && y+newY>0 && y+newY<src->Height)
-				{
-					TColor color=src->Canvas->Pixels[x+newX][y+newY];
-					int r=GetRValue(color);
-					int g=GetGValue(color);
-					int b=GetBValue(color);
-
-					dst->Canvas->Pixels[x][y]=TColor(RGB(r,g,b));
-				}  else{
-					dst->Canvas->Pixels[x][y]=clWhite;
-				}
-			}
-		}
-		Image2->Picture->Bitmap->Assign(dst);
-    }
 }
+//---------------------------------------------------------------------------
+
 //---------------------------------------------------------------------------
 
 void __fastcall TForm1::Image1MouseDown(
     TObject* Sender, TMouseButton Button, TShiftState Shift, int X, int Y)
 {
-    pt[cnt].X = X;
+    // PROFILUL UNEI LINII
+    Graphics::TBitmap* source = new Graphics::TBitmap;
+    Graphics::TBitmap* dstS = new Graphics::TBitmap;
+    source->Assign(Image1->Picture->Bitmap);
+    source->PixelFormat = Image1->Picture->Bitmap->PixelFormat;
+
+    RGBTRIPLE* linie =
+        (RGBTRIPLE*)
+            source->ScanLine[Y * (float)source->Height / Image1->Height];
+
+    Chart1->Series[0]->Clear();
+    Chart1->Series[1]->Clear();
+    Chart1->Series[2]->Clear();
+
+    for (int i = 0; i < 256; i++) {
+        Chart1->Series[0]->AddY(linie[i].rgbtRed, "", clRed);
+        Chart1->Series[1]->AddY(linie[i].rgbtGreen, "", clGreen);
+        Chart1->Series[2]->AddY(linie[i].rgbtBlue, "", clBlue);
+    }
+    Chart1->Series[0]->Active = true;
+    Chart1->Series[1]->Active = true;
+    Chart1->Series[2]->Active = true;
+
+	pt[cnt].X = X;
     pt[cnt].Y = Y;
     cnt++;
 }
 //---------------------------------------------------------------------------
-
